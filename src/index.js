61ca88eafec2f56383270d4e06e1199400b4bb8f
Alexander Danilowicz <alexander.j.danilowicz.19@dartmouth.edu>
2018-04-13 16:28:25 -0400

    finished starterpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('starting up!');
+// OLD IMPORT STYLE: const $ = require('jquery');
+// New import style, ES6 syntax
+import $ from 'jquery';
+import './style.scss';
+
+/* seperate function
+function increase_seconds() {
+  seconds++;
+  $('#main').html(`You've been on this page for ${seconds} seconds..`);
+} */
+
+const MILISECONDS = 1000;
+let seconds = 0;
+
+setInterval(() => {
+  seconds += 1;
+  // find the element with the id of main and
+  $('#main').html(`You've been on this page for ${seconds} seconds...`);
+}, MILISECONDS);
